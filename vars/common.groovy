799dfa601c7500613d9aead9e63532dea41e1d82
Alekhya <asri42080@gmail.com>
2023-02-15 22:36:02 +0530

    moving into CI

--- vars/common.groovy
+++ vars/common.groovy
@@ -2,11 +2,16 @@ def compile(){
     if(app_lang == "nodejs"){
         sh 'npm install'
         sh 'env'
-
     }
     if(app_lang == "maven"){
         sh 'mvn package'
     }
+    if(app_lang == "golang"){
+        sh  ' go mod init dispatch '
+        sh  'go get'
+        sh  'go build'
+    }
+
 }
 def unittests() {
     if (app_lang == "nodejs") {
@@ -19,11 +24,7 @@ def unittests() {
     if(app_lang == "python"){
         sh 'python3 -m unittest'
     }
-    if(app_lang == "golang"){
-    sh  ' go mod init dispatch '
-    sh  'go get'
-    sh  'go build'
-}
+
     def email(email_note) {
         mail bcc: '', body: "job failed -${JOB_BASE_NAME}\\n Jenkins URL - ${JOB_URL}", cc: '',
                 from: 'asri42080@gmail.com', replyTo: '', subject: "jenkins job failed -${JOB_BASE_NAME}", to: 'asri42080@gmail.com'
