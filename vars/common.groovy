48595b3960f6c23f020f46494912f914dc0a8e46
Alekhya <asri42080@gmail.com>
2023-02-16 20:04:04 +0530

    moving into CI

--- vars/common.groovy
+++ vars/common.groovy
@@ -26,12 +26,12 @@ def unittests() {
     if (app_lang == "golang") {
         sh 'go test'
     }
-
+}
     def email(email_note) {
         mail bcc: '', body: "job failed -${JOB_BASE_NAME}\n Jenkins URL - ${JOB_URL} \n Build Number -${BUILD_TAG} ", cc: '',
                 from: 'asri42080@gmail.com', replyTo: '', subject: "jenkins job failed -${JOB_BASE_NAME}", to: 'asri42080@gmail.com'
     }
 
-}
+
 
 
